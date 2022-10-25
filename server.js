30538af0d580ef5c7d674d1c7eb45f750c868d0e
hasnafar <hasnafarrukh@gmail.com>
2022-10-24 20:16:51 -0400

    Add middleware

--- server.js
+++ server.js
@@ -12,3 +12,12 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
