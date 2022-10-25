1d5e123dc9abb380a0bf971f9325b3eda014895e
hasnafar <hasnafarrukh@gmail.com>
2022-10-24 20:17:09 -0400

    turn on routes

--- server.js
+++ server.js
@@ -21,3 +21,22 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 
+
+// turn on routes 
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+
+
