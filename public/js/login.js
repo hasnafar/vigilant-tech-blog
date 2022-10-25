927ac97169ca137bcfb6353137714fbe6515c446
hasnafar <hasnafarrukh@gmail.com>
2022-10-24 20:18:59 -0400

    making dashboard the landing page

--- public/js/login.js
+++ public/js/login.js
@@ -13,7 +13,7 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/dashboard');
     } else {
       document.getElementById('pw-fail').classList.remove('d-none');
 
